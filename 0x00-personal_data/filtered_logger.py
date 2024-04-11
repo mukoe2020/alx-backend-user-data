be2d6a542fe38504ff1bc5f3105973b847d5b859
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-04-11 11:07:28 +0200

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ import re
 from typing import List
 from os import environ
 import logging
-
+import mysql.connector
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -37,3 +37,36 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.field, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
+
+
+def get_logger() -> logging.Logger:
+    """
+      Function that creates a custom logger, and
+      sets it format, level and gives it handler.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+      A function that uses mysql connector driver,
+      to connect to a database
+    """
+    usrname = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    passwrd = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    hostname = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    con = mysql.connector.connection.MySQLConnection(user=usrname,
+                                                     password=passwrd,
+                                                     host=hostname,
+                                                     database=db_name)
+    return con
+
