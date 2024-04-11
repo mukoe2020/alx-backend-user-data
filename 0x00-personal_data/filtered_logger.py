56dd3a8239b6f411913f35150f60b73e32004cba
memoe <memoemukoe@gmail.com>
2024-04-11 10:23:48 +0200

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+a function called filter_datum
+
+Arguments:
+fields: a list of strings representing all fields to obfuscate
+redaction: a string representing by what the field will be obfuscated
+message: a string representing the log line
+separator: a string representing by which character is
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """returns the log message obfuscated"""
+    return re.sub(r'(\b{}\b)'.format('|'.join(fields)), redaction, message)
