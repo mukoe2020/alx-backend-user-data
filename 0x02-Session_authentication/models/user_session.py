4a6f5d0a34297b8fa22dc7043c0a2543a2c32bde
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-04-18 17:38:09 +0200

    Session cookie

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+User session module
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """user session class"""
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """initializes user session"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
