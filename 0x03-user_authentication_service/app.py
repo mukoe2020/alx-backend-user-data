3037b8dbd35c9c4153d9d4d108814f13a73619ae
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-04-23 15:26:16 +0200

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,6 +18,21 @@ def hello() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """
+    POST /users
+    Register a user
+    Return: user email
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = Auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}) ,400
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
