d7335cc2765c457c7fe764ba5f2a2ab555b87e81
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-04-23 14:29:07 +0200

    hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
-"""
-In this task you will define a _hash_password method that takes
-in a password string arguments and returns bytes
-"""
+"""module contains hashing method"""
+
 import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.exc import NoResultFound
 
 
-def hash_password(password: str) -> bytes:
-    """
-    Hashes a password with bcrypt
-    """
+def _hash_password(password: str) -> bytes:
+    """hashes a password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers a user and hashes their password
+        then adds them to the database """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError('User {} already exists'.format(email))
+
