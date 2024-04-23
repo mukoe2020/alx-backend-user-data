d417f0f1ea4d428e0da591629bf121d355502df0
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-04-23 14:15:56 +0200

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+In this task you will define a _hash_password method that takes
+in a password string arguments and returns bytes
+"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password with bcrypt
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
