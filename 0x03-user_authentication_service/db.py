18821777c2c49f0ef99e8b786b8f4bda20090545
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-04-23 12:04:57 +0200

    add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,14 +34,22 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+       def add_user(self, email: str, hashed_password: str) -> User:
         """Add a user to the database
+        
+        Args:
+            email (str): The email of the user
+            hashed_password (str): The hashed password of the user
+        
+        Returns:
+            User: The newly created User object
         """
+        # Create a new User object
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        
         return user
-    
     def find_user_by(self, **kwargs) -> User:
         """
         Make sure that SQLAlchemy’s NoResultFound and InvalidRequestError 
